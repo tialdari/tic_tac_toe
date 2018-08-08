035388b91c1d9314a1a7bc6e83aca79d4f0e1d7b
Asia <asialuk98@gmail.com>
2018-08-08 23:59:49 +0200

    add the XOButton class and the images

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,11 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TicTacToe extends JFrame {
 
     JPanel p = new JPanel();
+    XOButton [] buttons  = new XOButton[9];
+
 
     public static void main(String [] args){
-        
+        new TicTacToe();
+    }
+
+    public TicTacToe(){
+        super("Tic Tac Toe");
+        setSize(400, 400);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 3));
+
+        for(int i = 0; i < 9; i ++){
+            buttons[i] = new XOButton();
+            p.add(buttons[i]);
+        }
+        add(p);
+
+        setVisible(true);
+
     }
 
 }
---
+++ src/XOButton.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class XOButton extends JButton {
+
+
+    ImageIcon X,O;
+    byte value == 0;
+    /*
+    0: nothing
+    1: X
+    2: O
+     */
+    public
+}
